3ab27e14ed70481cfe977aa00f76a3e3ff192280
jaforimamsourov <jaforimamsowrov@gmail.com>
2024-08-21 22:32:05 -0700

    javaScript Alert

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY)
 });
+
+alert("Welcome..!!❤️")
